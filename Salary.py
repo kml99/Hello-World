5dd2f1277d771e3c6edf5d2ed2d55639e9a33c9c
Kelly Lambert <you@example.com>
2024-03-17 16:41:31 +0000

    added additional user input

--- Salary.py
+++ Salary.py
@@ -1,4 +1,5 @@
-num_days = int(input("How many days did you work this month?"))
-pay_per_day = float(input("How much is your pay per day?"))     
+user_name = input("\nWhat is your name? ")
+num_days = int(input("\nHow many days did you work this month? "))
+pay_per_day = float(input("\nHow much is your pay per day? "))     
 salary = num_days * pay_per_day
-print("My salary for the month is USD:{}".format(salary))     
+print("\nMy salary for the month is USD:{}".format(salary))     
