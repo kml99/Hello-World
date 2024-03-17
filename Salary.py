c264e370236dd35f2e619214b19b9b7d295a2f9c
Kelly Lambert <you@example.com>
2024-03-17 16:08:56 +0000

    addedcomments

--- Salary.py
+++ Salary.py
@@ -1,4 +1,9 @@
+# input for workings days and pay per day
 num_days = int(input("How many days did you work this month?"))
-pay_per_day = float(input("How much is your pay per day?"))     
+pay_per_day = float(input("How much is your pay per day?"))   
+
+# salary calculation
 salary = num_days * pay_per_day
+
+# print out of information
 print("My salary for the month is USD:{}".format(salary))     
